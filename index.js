f3213a1f43e9d235a40587caec11b1b5551c3aea
bergi <bergi@axolotlfarm.org>
2015-10-22 23:06:37 +0200

    moved test code to rdf-test-graph and use rdf-graph-abstract as base class for Graph

--- index.js
+++ index.js
@@ -1,55 +1,5 @@
-var normalize = require('rdf-normalize')
-
-var rdf = {}
-
-rdf.encodeString = function (s) {
-  var out = ''
-  var skip = false
-  var _g1 = 0
-  var _g = s.length
-
-  while (_g1 < _g) {
-    var i = _g1++
-
-    if (!skip) {
-      var code = s.charCodeAt(i)
-
-      if (code >= 55296 && code <= 56319) {
-        var low = s.charCodeAt(i + 1)
-        code = (code - 55296) * 1024 + (low - 56320) + 65536
-        skip = true
-      }
-
-      if (code > 1114111) {
-        throw new Error('Char out of range')
-      }
-
-      var hex = '00000000'.concat((Number(code)).toString(16).toUpperCase())
-
-      if (code >= 65536) {
-        out += '\\' + hex.slice(-8)
-      } else {
-        if (code >= 127 || code <= 31) {
-          switch (code) {
-            case 9: out += '\\t'; break
-            case 10: out += '\\n'; break
-            case 13: out += '\\r'; break
-            default: out += '\\u' + hex.slice(-4); break
-          }
-        } else {
-          switch (code) {
-            case 34: out += '\\"'; break
-            case 92: out += '\\\\'; break
-            default: out += s.charAt(i); break
-          }
-        }
-      }
-    } else {
-      skip = !skip
-    }
-  }
-  return out
-}
+var rdf = require('rdf-graph-abstract')
+var util = require('util')
 
 rdf.NamedNode = function (iri) {
   this.interfaceName = 'NamedNode'
@@ -239,32 +189,16 @@ rdf.Quad.prototype.toTriple = function () {
   return new rdf.Triple(this.subject, this.predicate, this.object)
 }
 
-rdf.Graph = function (array) {
+rdf.Graph = function (other) {
   this.actions = []
-
-  Object.defineProperty(this, 'length', {
-    get: function () { return this._graph.length }
-  })
 
   this._graph = []
   this._gspo = {}
 
-  if (array) {
-    this.addArray(array)
-  }
+  rdf.AbstractGraph.call(this, rdf.Graph, other)
 }
 
-rdf.Graph.prototype.toString = function () {
-  return this._graph
-    .map(function (quad) {
-      return quad.toString()
-    })
-    .join('\n')
-}
-
-rdf.Graph.prototype.equals = function (other) {
-  return normalize(this) === normalize(other)
-}
+util.inherits(rdf.Graph, rdf.AbstractGraph)
 
 rdf.Graph.prototype.add = function (quad) {
   var i = rdf.Graph.index(quad)
@@ -288,108 +222,6 @@ rdf.Graph.prototype.addAction = function (action) {
   // TODO: implement me
 }
 
-rdf.Graph.prototype.addAll = function (other) {
-  var self = this
-
-  other.forEach(function (quad) {
-    self.add(quad)
-  })
-
-  return this
-}
-
-rdf.Graph.prototype.addArray = function (array) {
-  var self = this
-
-  array.forEach(function (quad) {
-    self.add(quad)
-  })
-
-  return this
-}
-
-rdf.Graph.prototype.clone = function () {
-  return new rdf.Graph(this)
-}
-
-rdf.Graph.prototype.difference = function (other) {
-  var difference = new rdf.Graph()
-
-  this.forEach(function (quad) {
-    if (!other.includes(quad)) {
-      difference.add(quad)
-    }
-  })
-
-  return difference
-};
-
-rdf.Graph.prototype.every = function (callback) {
-  return this._graph.every(callback)
-}
-
-rdf.Graph.prototype.filter = function (callback) {
-  return new rdf.Graph(this._graph.filter(callback))
-}
-
-rdf.Graph.prototype.forEach = function (callback) {
-  var self = this
-
-  this._graph.forEach(function (quad) {
-    callback(quad, self)
-  })
-}
-
-rdf.Graph.prototype.includes = function (quad) {
-  return this.match(quad.subject, quad.predicate, quad.object, quad.graph).length === 1
-}
-
-rdf.Graph.prototype.intersection = function (other) {
-  var intersection = new rdf.Graph()
-
-  this.forEach(function (quad) {
-    if (other.includes(quad)) {
-      intersection.add(quad)
-    }
-  })
-
-  return intersection
-}
-
-rdf.Graph.prototype.map = function (callback) {
-  var self = this
-
-  return this._graph.map(function (quad) {
-    return callback(quad, self)
-  })
-}
-
-rdf.Graph.prototype.match = function (subject, predicate, object, graph) {
-  return new rdf.Graph(this._graph.filter(function (quad) {
-    if (graph && (!quad.graph || !quad.graph.equals(graph))) {
-      return false
-    }
-
-    if (subject && !quad.subject.equals(subject)) {
-      return false
-    }
-
-    if (predicate && !quad.predicate.equals(predicate)) {
-      return false
-    }
-
-    if (object && !quad.object.equals(object)) {
-      return false
-    }
-
-    return true
-  }))
-}
-
-rdf.Graph.prototype.merge = function (other) {
-  return (new rdf.Graph(this.toArray()).addAll(other))
-}
-
 rdf.Graph.prototype.remove = function (quad) {
   var i = rdf.Graph.index(quad)
 
@@ -397,21 +229,6 @@ rdf.Graph.prototype.remove = function (quad) {
     delete this._gspo[i.g][i.s][i.p][i.o]
     this._graph.splice(this._graph.indexOf(quad), 1)
   }
-}
-
-rdf.Graph.prototype.removeMatches = function (subject, predicate, object, graph) {
-  var self = this
-  var matches = this.match(subject, predicate, object, graph)
-
-  matches.forEach(function (quad) {
-    self.remove(quad)
-  })
-
-  return this
-}
-
-rdf.Graph.prototype.some = function (callback) {
-  return this._graph.some(callback)
 }
 
 rdf.Graph.prototype.toArray = function () {
